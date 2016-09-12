ef695952e484618a134a331b8cf9a1a418058c0d
d2fong <d2fong@uwaterloo.ca>
2016-09-12 19:44:05 -0400

    Sets up store to be accessible from each component via the Provider

--- QuotationMachine/src/index.js
+++ QuotationMachine/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { quoteAppReducer } from  './reducers/Reducers';
 import App from './components/App';
 import './index.css';
 
+let store = createStore(quoteAppReducer)
 
 ReactDOM.render(
-  <App />,
+    <Provider store={store}>
+  		<App />
+  	</Provider>,
   document.getElementById('root')
 );
